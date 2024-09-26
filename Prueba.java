1c4ce9399113830d08385c06ef997899516a034a
omartinin <omartinin@gmail.com>
2024-09-26 11:07:31 +0200

    Muestra año y fecha de nacimiento, comida y grupo favoritos

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Rey Guijarro");
 		System.out.println("31 1993");
 		System.out.println("Pulpo a la gallega");
+		System.out.println("Dream Theater");
 
 	}
 
